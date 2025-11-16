375a215f1c8939ec4f9f6e5e13d4340759c02ca9
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-11-16 14:15:36 +0000

    Update holiday-countdown.js

--- holiday-countdown/holiday-countdown.js
+++ holiday-countdown/holiday-countdown.js
@@ -5,6 +5,11 @@ function createHolidayCountdown()
   console.log("Creating Holiday Countdown Timer");
   const title = document.getElementById("trip-name").value;
   console.log("Trip Name = "+title);
+  const trip-date = document.getElementById("trip-date").valueAsDate;
+  let today = new Date();
+  let secondsBetween = trip-date - today;
+  let daysBetween = Math.floor(secondsBetween / (1000 * 60 * 60 *24));
+  console.log("Days until the trip "+daysBetween);
 }
   
 function oldCode()  
