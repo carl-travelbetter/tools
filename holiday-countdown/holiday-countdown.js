7b99b6882358e27b8ae846c7f6c2b2a7cc4503e3
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-11-22 22:31:02 +0000

    Update holiday-countdown.js

--- holiday-countdown/holiday-countdown.js
+++ holiday-countdown/holiday-countdown.js
@@ -61,26 +61,39 @@ function displayCountdowns()
   //Clear the current displayed countdowns
   countdownList.innerHTML = "";
   state.countdownList.forEach(countdown => {
-    console.log("Title "+countdown.title);
-    console.log("tdate "+countdown.tdate);
     storedDate = new Date(countdown.tdate);
       
-     //Get today 
-    let today = new Date();
-    //Calculate the seconds between the date selected and today
-    let totalSecondsBetween = Math.floor((storedDate - today) / 1000);
-    //Calculate (rounding down) full days in those seconds 
-    let days = Math.floor(totalSecondsBetween / 86400);
-    //Calculate the days remaining in the total seconds (will be a real/float)
-    let daysRemaining = totalSecondsBetween % 86400;
-    //Calculate the full hours in the days remaining
-    let hours = Math.floor(daysRemaining / 3600);
-    //Calculate the hours remaining
-    let hoursRemaining = daysRemaining % 3600;
-    //Calculate the full minutes 
-    let minutes = Math.floor(hoursRemaining / 60);
-    //Calculate the remaining minutes which is in seconds
-    let seconds = hoursRemaining % 60;
+     //Work out how many years to go (for mutiplication factor of months) then add the difference in the month values;
+    //e.g. (2027 - 2025) * 12 = 24 months, June - December, 5 - 11 = -6, therefore 24 - 6 = 18 months
+    let monthsDiff = ((date.getYear() - today.getYear()) * 12) + (date.getMonth() - today.getMonth());
+    
+    //Work out the whole month count 
+    if (date.getDate() < today.getDate())
+    {
+      monthsDiff--;
+    }
+  
+    //If the monthDiff is set to -1 then move to 0
+    if (monthsDiff < 0)
+    {
+      monthsDiff = 0;
+    }
+    
+    //set a ms convert value
+    msPerDay = 1000 * 60 * 60 * 24;
+  
+    //Move on today by the months to go so only days left.
+    let tempDate = new Date();
+    tempDate.setMonth(today.getMonth()+monthsDiff);
+    
+    //calculate the total days remaining
+    let totalDaysRemaining = Math.floor((date - tempDate) / msPerDay);
+  
+    //calculate the weeks remaining
+    let weeks = Math.floor(totalDaysRemaining / 7);
+  
+    //calculate the remainder of days
+    let days = totalDaysRemaining % 7;
 
     //Output the countdown
     const countdownCard = document.createElement("div");
@@ -89,7 +102,7 @@ function displayCountdowns()
     countdownTitle.textContent = countdown.title;
     countdownCard.appendChild(countdownTitle);
     const countdownDays = document.createElement("p");
-    countdownDays.textContent = days+"days:"+hours+"hrs:"+minutes+"minutes:"+seconds+"seconds: to go.";
+    countdownDays.textContent = monthsDiff+" Months "+weeks+" Weeks "+days+" Days to go.";
     countdownCard.appendChild(countdownDays);
     countdownList.appendChild(countdownCard);
   });
