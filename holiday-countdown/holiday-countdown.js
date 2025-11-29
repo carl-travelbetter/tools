3a60f0e6f8443d18e3364b405fa886d58b491b55
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-11-29 21:49:56 +0000

    Update holiday-countdown.js

--- holiday-countdown/holiday-countdown.js
+++ holiday-countdown/holiday-countdown.js
@@ -138,28 +138,4 @@ function getOrdinalSuffix(n) {
 }
 
   
-function oldCode()  
-{
-/*  p.style.display = "none";
-document.getElementById("today").innerHTML = "Calculating Time...";
-
-let travelDay = document.getElementById("travelDate").valueAsDate;
-
-let x = setInterval(function()
-{
-let date1 = new Date();
-let millSecToGo = travelDay.getTime() - date1.getTime();
-
-
-// Time calculations for hours, minutes and seconds
-let days = Math.floor(millSecToGo / (1000 * 60 * 60 * 24));
-let hours = Math.floor((millSecToGo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-let minutes = Math.floor((millSecToGo % (1000 * 60 * 60)) / (1000 * 60));
-let seconds = Math.floor((millSecToGo % (1000 * 60)) / 1000);
-
-
-
-document.getElementById("today").innerHTML = days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
-}, 1000);*/
-}
 
