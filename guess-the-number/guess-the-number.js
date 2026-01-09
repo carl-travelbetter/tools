56e0859edab5aedeb1c3be897c8f3e4fa4219695
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-01-09 18:28:29 +0000

    Update guess-the-number.js

--- guess-the-number/guess-the-number.js
+++ guess-the-number/guess-the-number.js
@@ -9,16 +9,43 @@ let guessCount = 5;
 
 //load best score for user
 
+//Call method to generate first target number
+generateTargetNumber();
+
 //generate target number based on range
 function generateTargetNumber(top, bottom)
 {
   console.log("Generate Target Number");
+  tagrteNumber = Math.floor(Math.random() * (topOfRange+1));
+  console.log("Target Number "+targetNumber);
 }
 
 //submit a guess and see if it matches the target
 function submitGuess()
 {
   console.log("Submit Guess");
+  guess = document.getElementById("guess");
+  if (isNaN(guess))
+  {
+    alert("You Must Enter a number value to guess");
+    return;
+  }
+
+  if (guess == targetNumber)
+  {
+    alert("Congrats - You did it");
+    return;
+  }
+  
+  if (higherOrLower)
+  {
+    console.log("Answer too high - go lower");
+  }
+  else
+  {
+    console.log("Answer too low - go higher");
+  }
+  
 }
 
 //output the results of the latest guess
@@ -61,10 +88,25 @@ function decreaseGuessCount()
 function increaseNumberRange()
 {
   console.log("Increase Number Range");
+  topOfRange = Maths.floor(topOfRange+5);
+  console.log("New Top of Range = "+topOfRange);
+  const range = document.getElementById("range");
+  range.innerHTML = "";
+  range.innerHTML = "0.."+topOfRange;
 }
 
 //decrease number range
 function decreaseNumberRange()
 {
   console.log("Decrease Number Range");
+  if (topOfRange == 5)
+  {
+    alert("Range cannot go any lower");
+    return;
+  }
+  topOfRange = Maths.floor(topOfRange-5);
+  console.log("New Top of Range = "+topOfRange);
+  const range = document.getElementById("range");
+  range.innerHTML = "";
+  range.innerHTML = "0.."+topOfRange;
 }
