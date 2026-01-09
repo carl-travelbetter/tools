f0112563d2f9841af00e1f34948be9e5605daedb
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-01-09 16:55:34 +0000

    Create guess-the-number.js

---
+++ guess-the-number/guess-the-number.js
@@ -0,0 +1,70 @@
+console.log("Guess The Number");
+
+//iniitlise global variables
+let targetNumber = 0;
+let guess = 0;
+let topOfRange = 10;
+let bottomOfRange = 0;
+let guessCount = 5;
+
+//load best score for user
+
+//generate target number based on range
+function generateTargetNumber(top, bottom)
+{
+  console.log("Generate Target Number");
+}
+
+//submit a guess and see if it matches the target
+function submitGuess()
+{
+  console.log("Submit Guess");
+}
+
+//output the results of the latest guess
+function outputResults()
+{
+  console.log("Output Results");
+}
+
+//determine hgher or lower for output
+function higherOrLower()
+{
+  concole.log("Higher or Lower");
+}
+
+//share the results
+function shareResults()
+{
+  console.log("Share results");
+}
+
+//Challenge a friend
+function challengeAFriend()
+{
+  console.log("Challenge A Friend");
+}
+
+//increase guess count
+function increaseGuessCount()
+{
+  console.log("Increase Guess Count");
+}
+
+//decrease guess count
+function decreaseGuessCount()
+{
+  console.log("Decrease Guess Count");
+}
+
+//increase number range
+function increaseNumberRange()
+{
+  console.log("Increase Number Range");
+}
+
+//decrease number range
+function decreaseNumberRange()
+{
+  console.log("Decrease Number Range");
+}
