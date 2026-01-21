28220f670285b8c23f1fbf933bd5d5f19a56e111
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-01-21 23:03:55 +0000

    Update holiday-planner.js

--- neurodiverse-holiday-planner/holiday-planner.js
+++ neurodiverse-holiday-planner/holiday-planner.js
@@ -2,3 +2,33 @@ console.log("Travelbetter Holiday Planner");
 
 //Load saved data
 //Load Trips
+const TRIP_KEY = "tb_trips";
+let state = JSON.parse(localStorage.getItem(TRIP_KEY)) || {savedTripList: []};
+
+function loadTrips()
+{
+  console.log("Load Trips");
+
+  //Check if any trips are saved
+  if (savedTripList.length === 0)
+  {
+    console.log("No Trips Saved");
+    displayNoSavedTrips();
+  }
+  else
+  {
+    displaySavedTrips();
+  }
+}
+
+//Display a helpful message saying no saved trips and provide an option to create a trip or see help files
+function displayNoSavedTrips()
+{
+  console.log("No Saved Trips");
+}
+
+//Display the saved trips with summary data and provide a manage trip option
+function displaySavedTrips()
+{
+  console.log("Load Saved Trips");
+}
