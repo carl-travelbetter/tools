656355c7676c0afcf234bd03fb1f3920e6fa409b
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-01-24 14:29:23 +0000

    Update holiday-planner.js

--- neurodivergent-holiday-planner/holiday-planner.js
+++ neurodivergent-holiday-planner/holiday-planner.js
@@ -21,6 +21,39 @@ function loadTrips()
   }
 }
 
+//Load the create trip options when the user clicks on create new trip
+function loadCreateTripOptions()
+{
+  console.log("Display Create Trip");
+  document.getElementById("create-trip").hidden = false;
+}
+
+//create a new trip when the user hits save
+function createTrip()
+{
+  console.log("Creating new trip");
+  const trip = {};
+  //Grab each value and create a new trip object
+  
+  const tripTitle = document.getElementById("trip-title").value;
+  if (tripTitle == null)
+  {
+    console.log("Error - no trip title entered");
+    return;
+  }
+  else
+  {
+    trip.title = tripTitle;
+    trips.push(trip);
+    saveTrips();
+  }
+
+   
+  //Manage empty / optional inputs
+  //Create blanks in trip object
+  //load trip object to trips list and then call save option
+}
+
 //Display a helpful message saying no saved trips and provide an option to create a trip or see help files
 function displayNoSavedTrips()
 {
