ec74d717725beb4dee828b45c31c6b7c4a657e99
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-02-01 16:48:57 +0000

    Create munites-until.js

---
+++ minutes-until/munites-until.js
@@ -0,0 +1,13 @@
+info('Minutes Until Initialised');
+
+info('Creating Minutes Until Countdown');
+
+
+
+
+//Simple output to apply some useful information
+function info(message)
+{
+  console.log("Minutes Until - INFO:"+message); 
+}
+
