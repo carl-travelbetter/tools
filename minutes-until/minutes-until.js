691bfd5994883246c8167a778b26b8777f28a134
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-02-01 18:00:26 +0000

    Update minutes-until.js

--- minutes-until/minutes-until.js
+++ minutes-until/minutes-until.js
@@ -2,7 +2,22 @@ info('Minutes Until Initialised');
 
 info('Creating Minutes Until Countdown');
 
+const range = document.getElementById("minutes");
+const valueEl = document.getElementById("minutesValue");
+const unitEl = document.getElementById("minutesUnit");
 
+function updateMinutesLabel() {
+    info('Update Minutes Label');
+    const mins = Number(range.value);
+    valueEl.textContent = mins;
+    unitEl.textContent = mins === 1 ? "minute" : "minutes";
+  }
+
+  // Update live while dragging
+  range.addEventListener("input", updateMinutesLabel);
+
+  // Ensure correct label on page load
+  updateMinutesLabel();
 
 
 //Simple output to apply some useful information
