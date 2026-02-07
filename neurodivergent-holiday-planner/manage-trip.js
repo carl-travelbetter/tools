835e670dd87aae0d0f958ba3f4b172173980fb93
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-02-07 11:37:57 +0000

    Update manage-trip.js

--- neurodivergent-holiday-planner/manage-trip.js
+++ neurodivergent-holiday-planner/manage-trip.js
@@ -40,6 +40,10 @@ function displayFocusTrip()
         let tripDestination = document.createElement('p');
         tripDestination.textContent = "Destination "+trip.destination;
         outputCard.appendChild(tripDestination);
+        let tripDate = new Date(trip.travelDate);
+        const tripDateLabel = document.createElement("p");
+        tripDateLabel.textContent = "Travel Date: "+getTripDate(tripDate);
+        outputCard.appendChild(tripDateLabel);
         outputArea.appendChild(outputCard);
       }
       else
@@ -56,4 +60,30 @@ function displayFocusTrip()
     console.log("No Trips Found");
     //Take some action
   }
+}
+
+//Return a useable trip date
+function getTripDate(date)
+{
+  console.log("Get Trip Date String");
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+  let suffix = getOrdinalSuffix(date.getDate());
+  let tripDate = day+" "+date.getDate()+suffix+" "+month+" "+year;
+  return tripDate;
+}
+
+//Return the correct data suffix
+function getOrdinalSuffix(n) {
+  const lastDigit = n % 10;
+  const lastTwo = n % 100;
+
+  if (lastTwo >= 11 && lastTwo <= 13) return "th";
+  if (lastDigit === 1) return "st";
+  if (lastDigit === 2) return "nd";
+  if (lastDigit === 3) return "rd";
+  return "th";
 }
