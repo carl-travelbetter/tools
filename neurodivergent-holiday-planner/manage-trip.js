81dd108973ecd673d2c0edd37287f7e6c1e2fed9
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-02-08 21:38:12 +0000

    Update manage-trip.js

--- neurodivergent-holiday-planner/manage-trip.js
+++ neurodivergent-holiday-planner/manage-trip.js
@@ -73,9 +73,6 @@ function displayFocusTrip()
         editBtn.className = ('control-btn');
         editBtn.textContent = 'Edit';
         editBtn.addEventListener("click", () => {
-          //Get title and add to focus list then change page
-          console.log("Edit Button Clicked");
-          //Need to create a global stored variable for focussed trip and then call in new app
           window.open("/neurodivergent-holiday-planner/edit-trip.html");
         });
         editOptions.appendChild(editBtn);
