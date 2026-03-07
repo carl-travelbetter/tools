2d8e010e3e356e1eea1bd478110318952a0344d9
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-03-07 09:41:13 +0000

    Update find-accommodation.js

--- neurodivergent-holiday-planner/find-accommodation.js
+++ neurodivergent-holiday-planner/find-accommodation.js
@@ -233,7 +233,9 @@ function closeResults() {
   activeIndex = -1;
 }
 
-resultsEl.addEventListener("keydown", (e) => {
+
+
+getEl('whereto').addEventListener("keydown", (e) => {
   if (resultsEl.hidden) return;
 
   if (e.key === "ArrowDown") {
