8c0c2ab785e0b5aee09e1f773c34218e20964cfb
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-03-22 21:19:32 +0000

    Update food-lookup.js

--- weight-loss/food-lookup.js
+++ weight-loss/food-lookup.js
@@ -20,12 +20,7 @@ function bindEvents() {
 document.addEventListener("DOMContentLoaded", bindEvents);
 
 async function searchProducts(query) {
-  const url = `https://world.openfoodfacts.net/cgi/search.pl?search_terms=${encodeURIComponent(query)}
-  &search_simple=1
-  &action=process
-  &json=1
-  &page_size=10
-  &fields=product_name,brands,nutriments`;
+  const url = `https://world.openfoodfacts.net/cgi/search.pl?search_terms=${encodeURIComponent(query)}&search_simple=1&action=process&json=1&page_size=10&fields=product_name,brands,nutriments`;
 
   const res = await fetch(url);
   const data = await res.json();
