f3ddeafbc9cba5ee66b92805c3480601d666beb9
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-04-11 16:34:21 +0100

    Update walk-tracker.js

--- walking/walk-tracker.js
+++ walking/walk-tracker.js
@@ -40,6 +40,8 @@ document.addEventListener("DOMContentLoaded", bindEvents);
 function addWalk()
 {
   console.log('Add Walk');
+  getEl('walk-time').value = "";
+  getEl('walk-distance').value = "";
   getEl('add-walk').hidden = false;
 }
 
@@ -54,6 +56,12 @@ function openTargetControl()
 function submitWalk()
 {
   console.log('Submit Walk');
+  let walk = {};
+  walk.minutes = getEl('walk-time').value || 0;
+  walk.distance = getEl('walk-distance').value || 0;
+  walkList.walks.push(walk);
+  saveData();
+  displayLog();
 }
 
 //Cancel Adding a Walk
@@ -62,6 +70,82 @@ function cancelAddWalk()
   console.log('Cancel Add Walk');
 }
 
+//display walk log
+function displayLog()
+{
+  console.log('Display Log');
+  closeAll();
+  const walkLogDisplay = getEl('daily-log');
+  walkLogDisplay.innerHTML = "";
+
+  const list = document.createElement('ul');
+
+  //Create a header row for the list
+  const listHeader = document.createElement('li');
+  const listHeaderID = document.createElement('span');
+  listHeaderID.className = 'span-id';
+  listHeaderID.textContent = 'walk';
+  listHeader.appendChild(listHeaderID);
+  const listHeaderItem = document.createElement('span');
+  listHeaderItem.className = 'span-text';
+  listHeaderItem.textContent = 'Time(minutes)';
+  listHeader.appendChild(listHeaderItem);
+  const listHeaderValue = document.createElement('span');
+  listHeaderValue.className = 'span-value';
+  listHeaderValue.textContent = 'Distance(km)';
+  listHeader.appendChild(listHeaderValue);
+  const listHeaderAction = document.createElement('span');
+  listHeaderAction.className = 'span-action';
+  listHeaderAction.textContent = 'Delete Walk';
+  listHeader.appendChild(listHeaderAction);
+
+  list.appendChild(listHeader);
+
+  //Work through list of walks and add to list output
+  let idx = 0;
+  walkList.walks.forEach (walk => {
+    idx++;
+    let listItem = document.createElement('li');
+    //listItem.textContent = item.description+': '+item.calories;
+    let listID = document.createElement('span');
+    listID.className = 'span-id';
+    listID.textContent = ""+idx;
+    listItem.appendChild(listID);
+    let listMinutes = document.createElement('span');
+    listMinutes.className = 'span-text';
+    listMinutes.textContent = walk.minuntes;
+    listItem.appendChild(listMinutes);
+    let listDistance = document.createElement('span');
+    listDistance.className = 'span-value';
+    listDistance.textContent = walk.distance;
+    listItem.appendChild(listDistance);
+    let listDeleteControl = document.createElement('span');
+    listDeleteControl.className = 'span-action';
+    
+    //create a delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "❌";
+    deleteButton.setAttribute("data-label", idx-1);
+      //Make the button do something
+       //Make the button do something when clicked
+      deleteButton.addEventListener("click", () => {
+      
+      console.log("Delete Expense Button Clicked");
+      console.log("Delete ID = "+deleteButton.dataset.label);   
+      walkList.walks.splice(deleteButton.dataset.label, 1); 
+      saveData();  
+      //updateTracker();
+      displayLog();
+      });
+      listDeleteControl.appendChild(deleteButton);
+      listItem.appendChild(listDeleteControl);
+      list.appendChild(listItem);
+  });
+  
+  walkLogDisplay.appendChild(list);
+  walkLogDisplay.hidden = false;
+  
+}
 
 //Save data
 function saveData()
@@ -70,4 +154,26 @@ function saveData()
   localStorage.setItem(WALK_LIST_KEY, JSON.stringify(walkList));
   localStorage.setItem(WALK_TRACKING_DAY, JSON.stringify(walkTrackingDay));
   localStorage.setItem(WALK_TARGET_KEY, JSON.stringify(targetData));
+}
+
+//close all option cards
+function closeAll()
+{
+  console.log('close all non-control cards');
+  getEl('add-walk').hidden = true;
+  getEl('set-target').hidden = true;
+  getEl('progress-check').hidden = true;
+  getEl('daily-log').hidden = true;
+}
+
+//Start a new day
+function startNewDay()
+{
+  console.log('Start a new day');
+  calorieList.caloriesSpent = [];
+  saveData();
+  getEl('add-walk').hidden = true;
+  getEl('set-target').hidden = true;
+  getEl('progress-check').hidden = true;
+  getEl('daily-log').hidden = true;
 }
