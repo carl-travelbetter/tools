ec34478246cddedabbd0a4810995d555a95ba0c9
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-05-30 16:55:21 +0100

    Update Cross-The-Channel-With-A-Car.js

--- ferry-travel/Cross-The-Channel-With-A-Car.js
+++ ferry-travel/Cross-The-Channel-With-A-Car.js
@@ -19,7 +19,7 @@ fetch('CTCWAC.json')
 
 //Set events for button clicks in document (will be applied to all dom objects (pages) that call this js
 function bindEvents() {
-  getEl('submit-crossing-option')?.addEventListener("click", loadResult);
+  getEl('travelOption')?.addEventListener("select", loadResult);
 }
 
 //Ensure html bindings are not applied until the html structure is built
