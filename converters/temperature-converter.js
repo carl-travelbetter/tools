5eaafe038de722cd68c3014a7a2a19332da82a30
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-09-14 19:31:28 +0100

    Fix console log string format in temperature converter

--- converters/temperature-converter.js
+++ converters/temperature-converter.js
@@ -1,8 +1,8 @@
-console.log("temperature-converter.js");
+console.log('temperature-converter');
 
 //Import Dom utils
 import {getWrittenDate, getDuration, addDays} from "/lib/date-helper.js";
-import { getEl, getText, getDate} from "/lib/dom.js";
+import {getEl, getText, getDate} from "/lib/dom.js";
 
 function convertCelcius()
 {
