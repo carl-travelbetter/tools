13dc8756b1346cf55d9a9b7833f8e2566a75e680
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-09-14 19:26:52 +0100

    Add temperature conversion functions

--- converters/temperature-converter.js
+++ converters/temperature-converter.js
@@ -1,2 +1,21 @@
 console.log("temperature-converter.js");
 
+//Import Dom utils
+import {getWrittenDate, getDuration, addDays} from "/lib/date-helper.js";
+import { getEl, getText, getDate} from "/lib/dom.js";
+
+function convertCelcius()
+{
+  console.log('Converting Celcius');
+}
+
+function convertFahrenheit()
+{
+  console.log('Converting Fahrenheit');
+}
+
+function convertKelvin()
+{
+  console.log('Converting Kelvin');
+}
+          
