58693b9f60246ada95789a1a09263ce0b6abe974
carl-travelbetter <cmciver@travelbetter.co.uk>
2026-09-15 12:17:11 +0100

    Update temperature-converter.js

--- converters/temperature-converter.js
+++ converters/temperature-converter.js
@@ -4,6 +4,16 @@ console.log('temperature-converter');
 import {getWrittenDate, getDuration, addDays} from "/lib/date-helper.js";
 import {getEl, getText, getDate} from "/lib/dom.js";
 
+//Set events for button clicks in document (will be applied to all dom objects (pages) that call this js
+function bindEvents() {
+  getEl('celcius')?.addEventListener("keyup", convertCelcius);
+  getEl('fahrenheit')?.addEventListener("keyup", convertFahrenheit);
+  getEl('kelvin')?.addEventListener("keyup", convertKelvin);
+}
+
+//Ensure html bindings are not applied until the html structure is built
+document.addEventListener("DOMContentLoaded", bindEvents);
+
 function convertCelcius()
 {
   console.log('Converting Celcius');
